59e9f9cb5e38cee0b7fe0ac396fe0fe3f12334c5
Zamomako <robin-delannoy_student2021@wilder.school>
2021-06-21 09:11:15 +0200

    initialize first interaction between database and backend

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 const express = require('express');
+const { db } = require('./conf');
 
 const app = express();
 
-app.get('/characters', async (req, res) => {
-  res.status(404).send('Route not found! ');
+app.get('/answer', async (req, res) => {
+  const sql = 'SELECT title, band FROM answer';
+  const [results] = await db.query(sql);
+  res.json(results);
 });
 
-app.use('/', (req, res) => {
-  res.status(404).send('Route not found! ');
+app.get('/answer/:id', async (req, res) => {
+  const { id } = req.params;
+  const sql = 'SELECT title, band FROM answer WHERE id=?';
+  const sqlValues = [id];
+  const [results] = await db.query(sql, sqlValues);
+  res.json(results);
+});
+
+app.get('/samplesent', async (req, res) => {
+  const sql = 'SELECT answer_id FROM samplesent';
+  const [results] = await db.query(sql);
+  res.json(results);
+});
+
+app.post('/samplesent', async (req, res) => {
+  const { answer_id } = req.body;
+  const sql = 'INSERT INTO samplesent answer_id VALUES ?';
+  const sqlValues = [answer_id];
+
+  try {
+    const [results] = await db.query(sql, sqlValues);
+    res.status(201).json(results);
+  } catch (err) {
+    if (err.code === 'ER_DUP_ENTRY') {
+      // 409: Conflict
+      return res.status(409).send('This user already exists!');
+    }
+    if (err.code === 'ER_BAD_NULL_ERROR') {
+      // 422 : Unprocessable Entity
+      return res.status(422).send('Please fill all fields!');
+    }
+    res.status(500).send('Generic error message');
+  }
 });
 
 app.listen(5050, () => {
-  console.log('Terra Battle API now available on http://localhost:5050 !');
+  console.log('DB humming man run on http://localhost:5050 !');
 });
